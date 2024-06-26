4efcb6a42c2909443b62c0136009a3c0a49ae7c5
Olessia <olessiasoukhareva@sinkingship.ca>
2024-06-26 12:08:23 -0400

    Support for python3.10 for maya 2024.2

--- package.py
+++ package.py
@@ -30,6 +30,7 @@ private_build_requires = [
 
 variants = [
     ["maya-2024", "python-3.9", "maya_devkit-2024"],
+    ["maya-2024", "python-3.10", "maya_devkit-2024"],
 ]
 
 uuid = "repository.ChadVernon_cvwrap"
